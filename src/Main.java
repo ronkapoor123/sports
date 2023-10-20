dcccf81e860c34a04a59bb6e0f9e2723cd2c01a6
jordansexton-drajem <jsdrajem@gmail.com>
2023-10-20 10:24:00 -0400

    hi

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        hockey hockey1 = new hockey();
-        System.out.println(hockey1);
+        //hockey hockey1 = new hockey();
+        //System.out.println(hockey1);
 
         MlbTeamPayroll payroll1 = new MlbTeamPayroll();
         System.out.println(payroll1);
--- src/hockey.java
+++ src/hockey.java
@@ -1,13 +1,51 @@
-public class hockey {
-
-
+public class hockey extends SportsData{
 
     private Long forwardCash;
     private Long defenseCash;
     private Long goalieCash;
 
-    private String teamNames;
+    private String Team;
 
+    public hockey(String team, Long totalMoney, Long forwardCash, Long defenseCash, Long goalieCash, String team1) {
+        super(team, totalMoney);
+        this.forwardCash = forwardCash;
+        this.defenseCash = defenseCash;
+        this.goalieCash = goalieCash;
+        Team = team1;
+    }
 
+    public Long getForwardCash() {
+        return forwardCash;
+    }
+
+    public void setForwardCash(Long forwardCash) {
+        this.forwardCash = forwardCash;
+    }
+
+    public Long getDefenseCash() {
+        return defenseCash;
+    }
+
+    public void setDefenseCash(Long defenseCash) {
+        this.defenseCash = defenseCash;
+    }
+
+    public Long getGoalieCash() {
+        return goalieCash;
+    }
+
+    public void setGoalieCash(Long goalieCash) {
+        this.goalieCash = goalieCash;
+    }
+
+    @Override
+    public String getTeam() {
+        return Team;
+    }
+
+    @Override
+    public void setTeam(String team) {
+        Team = team;
+    }
 }
 

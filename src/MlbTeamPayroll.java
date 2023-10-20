c51c4e830acdd940aa03a21e8c8bf45c0e2ba990
ronkapoor123 <145025068+ronkapoor123@users.noreply.github.com>
2023-10-20 10:11:25 -0400

    Update

--- src/MlbTeamPayroll.java
+++ src/MlbTeamPayroll.java
@@ -1,3 +1,21 @@
-public class MlbTeamPayroll {
+public class MlbTeamPayroll extends SportsData{
+    // fields
+    private int rank;
+    private long payroll;
+    private long injuredreserve;
+    private long retained;
+
+
+    //www.spotrac.com/mlb/payroll//;
+
+    // constructors
+
+    // setter/getters
+
+    // methods
+    public String toString() {
+        String description = "Team named" + teamNames + " is ranked " + rank;
+        return description;
+    }
 
 }
--- src/SportsData.java
+++ src/SportsData.java
@@ -1,9 +1,26 @@
 public class SportsData {
 
-    String Team;
+    private String Team;
+    private Long totalMoney;
 
+    public SportsData(String team, Long totalMoney) {
+        Team = team;
+        this.totalMoney = totalMoney;
+    }
 
-    Long totalMoney;
+    public String getTeam() {
+        return Team;
+    }
 
+    public void setTeam(String team) {
+        Team = team;
+    }
 
+    public Long getTotalMoney() {
+        return totalMoney;
+    }
+
+    public void setTotalMoney(Long totalMoney) {
+        this.totalMoney = totalMoney;
+    }
 }
